c67b474a5739d318648ad8a7914647a1d7e6a1d9
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-10 00:20:58 +0900

    feat(main): Create class 'Vector<T, 3>' and constructors

--- cpu/includes/Vector/Vector3.hpp
+++ cpu/includes/Vector/Vector3.hpp
@@ -4,11 +4,13 @@
 #include <limits>
 #include <type_traits>
 
+#include "Vector.hpp"
+
 namespace BLAS {
     //!
     //! \brief 3D vector class.
     //!
-    //! This class defines 3D vector data structure and associated operations.
+    //! This class defines 3D vector and associated operations.
     //!
     //! \tparam T - Type of the element
     //!
@@ -19,13 +21,44 @@ namespace BLAS {
                 "Vector only can be instantiated with floating point types.");
 
         //! Constructs 3D vector filled with zeros.
-        constexpr Vector()
+        constexpr Vector() : _x{ 0 }, _y{ 0 }, _z{ 0 }
+        {
+            // Do nothing
+        }
 
+        //! Constructs 3D vector whose elements are initialized with given values.
+        constexpr Vector(T x, T y, T z) : _x{ x }, _y{ y }, _z{ z }
+        {
+            // Do nothing
+        }
+
+        //! Constructs 3D vector with initializer list.
+        template <typename U>
+        Vector(const std::initializer_list<U>& list);
+
+        //! Copy constructor.
+        constexpr Vector(const Vector& v) : _x{ v._x }, _y{ v._y }, _z{ v._z }
+        {
+            // Do nothing
+        }
+
+        //! Move constructor.
+        constexpr Vector(Vector&& v) noexcept : _x{ v._x }, _y{ v._y }, _z{ v._z }
+        {
+            // Do nothing
+        }
+
+        //! Default destructor.
+        ~Vector() = default;
 
     private:
-        T x;
-        T y;
-        T z;
+        T _x;
+        T _y;
+        T _z;
     };
+
 }
+
+#include "Vector3-Impl.hpp"
+
 #endif //BLAS_VECTOR3_HPP

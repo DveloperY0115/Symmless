3ce6c47faef77a6fa1ee4978a249209c10db2404
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-10 00:22:33 +0900

    feat(main): Define Vector<T, 3> constructor with initializer list

--- cpu/includes/Vector/Vector3-Impl.hpp
+++ cpu/includes/Vector/Vector3-Impl.hpp
@@ -5,5 +5,15 @@
 #ifndef BLAS_VECTOR3_IMPL_HPP
 #define BLAS_VECTOR3_IMPL_HPP
 
+#include "Vector3.hpp"
+
+namespace BLAS {
+
+    template<typename T>
+    template<typename U>
+    Vector<T, 3>::Vector(const std::initializer_list<U>& list) {
+        Set(list);
+    }
+
 
 #endif //BLAS_VECTOR3_IMPL_HPP

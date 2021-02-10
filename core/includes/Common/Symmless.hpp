00f1d37d8009dded071c629a9629ada90ff32954
SeungWoo Yoo <dreamy1534@gmail.com>
2021-02-10 18:23:41 +0900

    feat(main): Add project header

---
+++ core/includes/Common/Symmless.hpp
@@ -0,0 +1,16 @@
+#ifndef SYMMLESS_SYMMLESS_HPP 
+#define SYMMLESS_SYMMLESS_HPP
+
+namespace Symmless {
+
+#if defined(__NVCC__) || (defined(__clang__) && defined(__CUDA__))
+#define SYMMLESS_HOST_DEVICE __forceinline__ __device__ __host__
+#define SYMMLESS_DEVICE __forceinline__ __device__
+#elif defined(__CUDACC_RTC__)
+#define SYMMLESS_HOST_DEVICE __forceinline__ __device__
+#define SYMMLESS_DEVICE __forceinline__ __device__
+#else
+#define SYMMLESS_HOST_DEVICE inline
+#endif
+}
+#endif //SYMMLESS_SYMMLESS_HPP 

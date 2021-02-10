8499a420a0d5b7450c75d0bfe2167aa5b65bd859
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-10 11:41:36 +0900

    feat(main): Add test for default constructor of class 'Vector3'

--- cpu/tests/unitTests/test_Vector3.cpp
+++ cpu/tests/unitTests/test_Vector3.cpp
@@ -2,8 +2,15 @@
 // Created by 유승우 on 2021/02/09.
 //
 
+#include "unitTest.hpp"
 #include <Vector/Vector3.hpp>
 
-int main() {
+using namespace BLAS;
 
+TEST(Vector3, Constructors) {
+    //! Default constructor
+    Vector<float, 3> vec = Vector<float, 3>();
+    EXPECT_FLOAT_EQ(0.0f, vec._x);
+    EXPECT_FLOAT_EQ(0.0f, vec._y);
+    EXPECT_FLOAT_EQ(0.0f, vec._z);
 }

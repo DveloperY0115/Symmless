20bf528c7b713d6d840a01de80989c18d5ce7ef9
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-10 14:20:21 +0900

    feat(main): Implement unit-test for constructors of Vector3

--- cpu/tests/unitTests/test_Vector3.cpp
+++ cpu/tests/unitTests/test_Vector3.cpp
@@ -9,7 +9,31 @@ using namespace BLAS;
 
 TEST(Vector3, Constructors) {
     //! Default constructor
-    Vector<float, 3> vec = Vector<float, 3>();
+    Vector<float, 3> vec_default_constructor = Vector<float, 3>();
+    EXPECT_FLOAT_EQ(0.0f, vec_default_constructor._x);
+    EXPECT_FLOAT_EQ(0.0f, vec_default_constructor._y);
+    EXPECT_FLOAT_EQ(0.0f, vec_default_constructor._z);
+
+    //! Constructor with initial values
+    Vector<float, 3> vec_initialized_with_params = Vector<float, 3>(3.0f, 2.0f, -1.0f);
+    EXPECT_FLOAT_EQ(3.0f, vec_initialized_with_params._x);
+    EXPECT_FLOAT_EQ(2.0f, vec_initialized_with_params._y);
+    EXPECT_FLOAT_EQ(-1.0f, vec_initialized_with_params._z);
+
+    //! Constructor with initializer list
+    Vector<float, 3> vec_initialized_with_list = { 1.0f, 1.0f, 1.0f };
+    EXPECT_FLOAT_EQ(1.0f, vec_initialized_with_list._x);
+    EXPECT_FLOAT_EQ(1.0f, vec_initialized_with_list._y);
+    EXPECT_FLOAT_EQ(1.0f, vec_initialized_with_list._z);
+
+    //! Copy constructor
+    Vector<float, 3> vec_copy_constructed = Vector<float, 3>(vec_initialized_with_params);
+    EXPECT_FLOAT_EQ(3.0f, vec_copy_constructed._x);
+    EXPECT_FLOAT_EQ(2.0f, vec_copy_constructed._y);
+    EXPECT_FLOAT_EQ(-1.0f, vec_copy_constructed._z);
+
+    //! Move constructor
+}
     EXPECT_FLOAT_EQ(0.0f, vec._x);
     EXPECT_FLOAT_EQ(0.0f, vec._y);
     EXPECT_FLOAT_EQ(0.0f, vec._z);

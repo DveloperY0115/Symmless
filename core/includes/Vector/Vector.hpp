8283bd72acc55fc8fc82cd60f07402d20d890316
dveloperY0115 <dreamy1534@gmail.com>
2021-05-23 22:29:59 +0900

    refactor(main): Modify comments

--- core/includes/Vector/Vector.hpp
+++ core/includes/Vector/Vector.hpp
@@ -7,14 +7,14 @@
 #include <type_traits>
 
 namespace Symmless {
-    //!
-    //! \brief Generalized vector class.
-    //!
-    //! This class defines a vector of size \p N and associated operations.
-    //!
-    //! \tparam T - Type of the element
-    //! \tparam N - Number of elements
-    //!
+    //
+    // \brief Generalized vector class.
+    //
+    // This class defines a vector of size \p N and associated operations.
+    //
+    // \tparam T - Type of the element
+    // \tparam N - Number of elements
+    //
     template<typename T, std::size_t N>
     class Vector final {
     public:
@@ -25,32 +25,30 @@ namespace Symmless {
 
         using ContainerType = std::array<T, N>;
 
-        //! Constructs a vector filled with zeros.
+        // Constructs a vector filled with zeros.
         SYMMLESS_HOST_DEVICE 
         Vector();
 
-        //! Constructs a vector with multiple parameters.
+        // Constructs a vector with multiple parameters.
         template <typename... Params>
         SYMMLESS_HOST_DEVICE 
         explicit Vector(Params... params);
 
-        //! Constructs a vector with initializer list.
+        // Constructs a vector with initializer list.
         template <typename U>
         SYMMLESS_HOST_DEVICE 
         Vector(const std::initializer_list<U>& list);
 
-        //! Copy constructor.
+        // Copy constructor.
         SYMMLESS_HOST_DEVICE 
         Vector(Vector& v);
 
-        //! Copy asssignment operator.
+        // Copy asssignment operator.
         SYMMLESS_HOST_DEVICE
         Vector& operator=(const Vector& v);
 
-
-
     private:
-        std::array<T, N> _elements;
+        ContainerType _elements;
     };
 }
 
--- core/includes/Vector/Vector3.hpp
+++ core/includes/Vector/Vector3.hpp
@@ -8,138 +8,138 @@
 #include "Vector.hpp"
 
 namespace Symmless {
-    //!
-    //! \brief 3D vector class.
-    //!
-    //! This class defines 3D vector and associated operations.
-    //!
-    //! \tparam T - Type of the element
-    //!
+    //
+    // \brief 3D vector class.
+    //
+    // This class defines 3D vector and associated operations.
+    //
+    // \tparam T - Type of the element
+    //
     template<typename T>
     class Vector<T, 3> final {
     public:
         static_assert(std::is_floating_point<T>::value,
                 "Vector only can be instantiated with floating point types.");
 
-        //! Constructs 3D vector filled with zeros.
+        // Constructs 3D vector filled with zeros.
         SYMMLESS_HOST_DEVICE
-        constexpr Vector() : _x{ 0 }, _y{ 0 }, _z{ 0 }
+        constexpr Vector() : _x{ 0.0 }, _y{ 0.0 }, _z{ 0.0 }
         {
             // Do nothing
         }
 
-        //! Constructs 3D vector whose elements are initialized with given values.
+        // Constructs 3D vector whose elements are initialized with given values.
         SYMMLESS_HOST_DEVICE
         constexpr Vector(T x, T y, T z) : _x{ x }, _y{ y }, _z{ z }
         {
             // Do nothing
         }
 
-        //! Constructs 3D vector with initializer list.
+        // Constructs 3D vector with initializer list.
         template <typename U>
         SYMMLESS_HOST_DEVICE
         Vector(const std::initializer_list<U>& list);
 
-        //! Copy constructor.
+        // Copy constructor.
         SYMMLESS_HOST_DEVICE
         constexpr Vector(const Vector& v) : _x{ v._x }, _y{ v._y }, _z{ v._z }
         {
             // Do nothing
         }
 
-        //! Move constructor.
+        // Move constructor.
         SYMMLESS_HOST_DEVICE
         constexpr Vector(Vector&& v) noexcept : _x{ v._x }, _y{ v._y }, _z{ v._z }
         {
             // Do nothing
         }
 
-        //! Default destructor.
+        // Default destructor.
         SYMMLESS_HOST_DEVICE
         ~Vector() = default;
 
-        //! Copy assignment operator.
+        // Copy assignment operator.
         SYMMLESS_HOST_DEVICE
         Vector& operator=(const Vector& v);
 
-        //! Move assignment operator.
+        // Move assignment operator.
         SYMMLESS_HOST_DEVICE
         Vector& operator=(const Vector&& v) noexcept;
 
-        //! Set all x,y, z components to \p s.
+        // Set all x,y, z components to \p s.
         SYMMLESS_HOST_DEVICE
         void Set(T s);
 
-        //! Set x, y, z components with each given values.
+        // Set x, y, z components with each given values.
         SYMMLESS_HOST_DEVICE
         void Set(T x, T y, T z);
 
-        //! Set x, y, z components with given initializer list.
+        // Set x, y, z components with given initializer list.
         template <typename U>
         SYMMLESS_HOST_DEVICE
         void Set(const std::initializer_list<U>& list);
 
-        //! Set x, y, z components with elements of given vector \p v.
+        // Set x, y, z components with elements of given vector \p v.
         SYMMLESS_HOST_DEVICE
         void Set(const Vector& v);
 
-        //! Set x, y, z to zero.
+        // Set x, y, z to zero.
         SYMMLESS_HOST_DEVICE
         void SetZero();
 
-        //! Calculate L2 norm of this vector
+        // Calculate L2 norm of this vector
         SYMMLESS_HOST_DEVICE
         float Length();
 
-        //! Normalize this vector.
+        // Normalize this vector.
         SYMMLESS_HOST_DEVICE
         void Normalize();
 
-        //! Element-wise scalar addition
+        // Element-wise scalar addition
         SYMMLESS_HOST_DEVICE
         Vector Add(T c) const;
 
-        //! Vector addition
+        // Vector addition
         SYMMLESS_HOST_DEVICE
         Vector Add(const Vector& v) const;
 
-        //! Element-wise scalar subtraction
+        // Element-wise scalar subtraction
         SYMMLESS_HOST_DEVICE
         Vector Sub(T c) const;
 
-        //! Vector subtraction
+        // Vector subtraction
         SYMMLESS_HOST_DEVICE
         Vector Sub(const Vector& v) const;
 
-        //! Element-wise scalar multiplication
+        // Element-wise scalar multiplication
         SYMMLESS_HOST_DEVICE
         Vector Mul(T c) const;
 
-        //! Element-wise vector multiplication
+        // Element-wise vector multiplication
         SYMMLESS_HOST_DEVICE
         Vector Mul(const Vector& v) const;
 
-        //! Element-wise scalar division
+        // Element-wise scalar division
         SYMMLESS_HOST_DEVICE
         Vector Div(T c) const;
 
-        //! Element-wise vector division
+        // Element-wise vector division
         SYMMLESS_HOST_DEVICE
         Vector Div(const Vector& v) const;
 
-        //! Dot product
+        // Dot product
         SYMMLESS_HOST_DEVICE
         T Dot(const Vector& v) const;
 
-        //! Cross product
+        // Cross product
         SYMMLESS_HOST_DEVICE
         Vector Cross(const Vector& v) const;
 
-        //! Right element-wise scalar subtraction
+        // Right element-wise scalar subtraction
         SYMMLESS_HOST_DEVICE
         Vector RSub(T c) const;
 
-        //! Right element-wise vector subtraction
+        // Right element-wise vector subtraction
         SYMMLESS_HOST_DEVICE
         Vector RSub(const Vector& v) const;
     public:
